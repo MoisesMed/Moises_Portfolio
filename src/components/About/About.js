a7e49e5d132420a330b6e3046c2ba74ab94f3961
MoisesMed <br.moises@hotmail.com>
2021-07-26 21:42:05 -0300

    sobre

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -15,13 +15,13 @@ export default function About() {
             <Col>
                 <Row className="textoSobreTitle"><text>Sobre</text></Row>
                 <Row className="textoSobre">
-                    <text>Meu nome é Moisés Medeiros Matos, tenho 22 anos e faço faculdade de Sistemas da Informação.Sou desenvolvedor Web e Mobile, técnico de informática e amante de tecnologia.</text>
+                    <text>Meu nome é Moisés Medeiros Matos, tenho 22 anos e faço faculdade de Sistemas da Informação. Sou desenvolvedor Web e Mobile, técnico de informática e amante de tecnologia.</text>
                 </Row>
                 <Row className="textoSobre">
                     <text>Por conta da faculdade, comecei a programar em C++ e Java no começo de 2019. Em 2020, mudei para JavaScript, e logo depois arrumei meu primeiro emprego como desenvolvedor Front End.</text>
                 </Row>
                 <Row className="textoSobre">
-                    <text>Meu primeiro emprego foi como estagiário de desenvolvimento Web utilizando JavaScript, HTML, CSS, React e SQL. Sigo com foco nessas stacks desde então.</text>
+                    <text>Nesse emprego, fui estagiário de desenvolvimento Web e trabalhei com JavaScript, HTML, CSS, React e SQL. Desde então, sigo com foco nessas stacks.</text>
                 </Row>
             </Col>
         </Container>
