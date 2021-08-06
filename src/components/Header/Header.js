f1ddb7deb81fdc37430ae549159fd68f85cf18a5
MoisesMed <br.moises@hotmail.com>
2021-08-06 01:32:09 -0300

    responsive navbar

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,8 +2,9 @@
 import React, { useState, useEffect } from "react";
 import {
     Container,
-    Col, Row
+    Col, Row, Nav, NavDropdown
 } from "react-bootstrap";
+import Navbar from 'react-bootstrap/Navbar'
 import "../Tecnologies/Tecnologies"
 
 import "./styles.css";
@@ -11,37 +12,64 @@ import "./styles.css";
 export default function Header() {
 
     return (
-        <Container className="containerMain" id="cabecalho">
-            <Row>
-                <Col className="nome">
-                    <a href="#cabecalho" style={{ color: "black" }}>
-                        Moisés Medeiros
-                    </a>
-                </Col>
-            </Row>
-            <Row>
-                <Col className="sobre" style={{ padding: 0, marginRight: 10 }}>
-                    <a href="#sobre" style={{ color: "black" }}>
-                        Sobre
-                    </a>
-                </Col>
-                <Col className="sobre" style={{ padding: 0, marginRight: 10 }}>
-                    <a href="#tecnologias" style={{ color: "black" }}>
-                        Habilidades
-                    </a>
-                </Col>
-                <Col className="sobre" style={{ padding: 0, marginRight: 10 }}>
-                    <a href="#certificacoes" style={{ color: "black" }}>
-                        Certificações
-                    </a>
-                </Col>
-                <Col className="sobre" style={{ padding: 0, marginRight: 10 }}>
-                    <a href="#projetos" style={{ color: "black" }}>
-                        Projetos
-                    </a>
-                </Col>
-            </Row>
-        </Container>
+        // <Container className="containerMain" id="cabecalho">
+        //     <Row>
+        //         <Col className="nome">
+        //             <a href="#cabecalho" style={{ color: "black" }}>
+        //                 Moisés Medeiros
+        //             </a>
+        //         </Col>
+        //     </Row>
+        //     <Row>
+        //         <Col className="sobre" style={{ padding: 0, marginRight: 10 }}>
+        //             <a href="#sobre" style={{ color: "black" }}>
+        //                 Sobre
+        //             </a>
+        //         </Col>
+        //         <Col className="sobre" style={{ padding: 0, marginRight: 10 }}>
+        //             <a href="#tecnologias" style={{ color: "black" }}>
+        //                 Habilidades
+        //             </a>
+        //         </Col>
+        //         <Col className="sobre" style={{ padding: 0, marginRight: 10 }}>
+        //             <a href="#certificacoes" style={{ color: "black" }}>
+        //                 Certificações
+        //             </a>
+        //         </Col>
+        //         <Col className="sobre" style={{ padding: 0, marginRight: 10 }}>
+        //             <a href="#projetos" style={{ color: "black" }}>
+        //                 Projetos
+        //             </a>
+        //         </Col>
+        //     </Row>
+        // </Container>
+
+        // <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark" className="NavBar">
+        //     <Container>
+        //         <Navbar.Brand href="#cabecalho" className="NavBarItemTitle">Moisés Medeiros</Navbar.Brand>
+        //         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        //         <Navbar.Collapse id="responsive-navbar-nav"  >
+        //             <Nav className="me-auto" >
+        //                 <Nav.Link href="#sobre" className="NavBarItem" >Sobre</Nav.Link>
+        //                 <Nav.Link href="#tecnologias" className="NavBarItem" >Habilidades</Nav.Link>
+        //                 <Nav.Link href="#certificacoes" className="NavBarItem" >Certificações</Nav.Link>
+        //                 <Nav.Link href="#projetos" className="NavBarItem" >Projetos</Nav.Link>
+        //             </Nav>
+        //         </Navbar.Collapse>
+        //     </Container>
+        // </Navbar>
+        < Navbar className="NavBar" bg="light" expand="lg" >
+            <Navbar.Brand href="#cabecalho" className="NavBarItemTitle">Moisés Medeiros</Navbar.Brand>
+            <Navbar.Toggle aria-controls="navbarScroll" />
+            <Navbar.Collapse id="navbarScroll">
+                <Nav className="me-auto" >
+                    <Nav.Link href="#sobre" className="NavBarItem" >Sobre</Nav.Link>
+                    <Nav.Link href="#tecnologias" className="NavBarItem" >Habilidades</Nav.Link>
+                    <Nav.Link href="#certificacoes" className="NavBarItem" >Certificações</Nav.Link>
+                    <Nav.Link href="#projetos" className="NavBarItem" >Projetos</Nav.Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar >
     );
 };
 
